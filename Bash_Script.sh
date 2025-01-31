e080933a0eee47afbc2e9af7b5b523880d389fad
Nino Vardè <162322782+ninovarde@users.noreply.github.com>
2025-01-31 14:46:35 +0000

    Update Bash_Script.sh

--- Bash_Script.sh
+++ Bash_Script.sh
@@ -3,7 +3,10 @@
 echo "Make sure that the specified path is the correct one !"
 
 # SPECIFY THE LOCATION WHERE YOU WOULD LIKE THE FOLDER TO BE CREATED:
-path=/choose/the/location
+path=/home/ABINFO/Desktop
 
 # Make the directory 'Varde_AbInfo' in the specified location
 mkdir $path/Varde_AbInfo
+
+script_directory=$(dirname "$0")
+mv $script_directory/Bash_Script.sh $path/Varde_AbInfo
