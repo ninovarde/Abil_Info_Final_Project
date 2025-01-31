03873bb25d31fa73fcb7310046b83db90d0958aa
Nino Vardè <162322782+ninovarde@users.noreply.github.com>
2025-01-31 18:35:30 +0100

    Update Bash_Script.sh

--- Bash_Script.sh
+++ Bash_Script.sh
@@ -16,4 +16,5 @@ mv $script_directory/Bash_Script.sh $path/Varde_AbInfo
 wget -P $path/Varde_AbInfo https://raw.githubusercontent.com/MilenaValentini/TRM_Dati/refs/heads/main/Nemo_6670.dat
 wget -P $path/Varde_AbInfo https://raw.githubusercontent.com/ninovarde/Abil_Info_Final_Project/refs/heads/main/data_analysis.py
 
+# Run the Python script to make the data analysis
 python3 $path/Varde_AbInfo/data_analysis.py
