2516e82d1658fadba8f16c3482507bf5ccaedc8f
Nino Vardè <162322782+ninovarde@users.noreply.github.com>
2025-01-31 18:49:32 +0100

    Update Bash_Script.sh

--- Bash_Script.sh
+++ Bash_Script.sh
@@ -1,9 +1,9 @@
-!/bin/bash
-
 echo "Make sure that the specified path is the correct one !"
 
 # SPECIFY THE LOCATION WHERE YOU WOULD LIKE THE FOLDER TO BE CREATED:
 path=/choose/the/location
+
+
 
 # Make the directory 'Varde_AbInfo' in the specified location
 mkdir $path/Varde_AbInfo
@@ -12,9 +12,15 @@ mkdir $path/Varde_AbInfo
 script_directory=$(dirname "$0")
 mv $script_directory/Bash_Script.sh $path/Varde_AbInfo
 
+
+
 # Download the data and the Python script for data analysis from the given links 
 wget -P $path/Varde_AbInfo https://raw.githubusercontent.com/MilenaValentini/TRM_Dati/refs/heads/main/Nemo_6670.dat
 wget -P $path/Varde_AbInfo https://raw.githubusercontent.com/ninovarde/Abil_Info_Final_Project/refs/heads/main/data_analysis.py
 
+
+
 # Run the Python script to make the data analysis
+chmod +x $path/Varde_AbInfo/data_analysis.py
 python3 $path/Varde_AbInfo/data_analysis.py
+
