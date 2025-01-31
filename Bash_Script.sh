5aa7aec54471cc48c04d888447c2d02c3354d690
Nino Vardè <162322782+ninovarde@users.noreply.github.com>
2025-01-31 14:48:33 +0100

    Create Bash_Script.sh

---
+++ Bash_Script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Make sure that the specified path is the correct one !"
+
+# SPECIFY THE LOCATION WHERE YOU WOULD LIKE THE FOLDER TO BE CREATED:
+path=/choose/the/location
+
+# Make the directory 'Varde_AbInfo' in the specified location
+mkdir $path/Varde_AbInfo
