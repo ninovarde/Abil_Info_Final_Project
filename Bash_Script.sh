ee897fec564b658a621d06ac4cb7a3d2b60be357
Nino Vardè <162322782+ninovarde@users.noreply.github.com>
2025-01-31 15:54:49 +0100

    Update Bash_Script.sh

--- Bash_Script.sh
+++ Bash_Script.sh
@@ -3,10 +3,16 @@
 echo "Make sure that the specified path is the correct one !"
 
 # SPECIFY THE LOCATION WHERE YOU WOULD LIKE THE FOLDER TO BE CREATED:
-path=/home/ABINFO/Desktop
+path=/choose/the/location
 
 # Make the directory 'Varde_AbInfo' in the specified location
 mkdir $path/Varde_AbInfo
 
+# Move this script to the created directory
 script_directory=$(dirname "$0")
 mv $script_directory/Bash_Script.sh $path/Varde_AbInfo
+
+# Download the data from the given link 
+wget -P $path/Varde_AbInfo https://raw.githubusercontent.com/MilenaValentini/TRM_Dati/refs/heads/main/Nemo_6670.dat
+
+
